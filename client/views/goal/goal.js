6eaa4b3c0cf9a3fe15b2a767f611dbd03375d531
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-24 11:20:53 -0500

    task form on goals page  hides appropriately

--- client/views/goal/goal.js
+++ client/views/goal/goal.js
@@ -5,6 +5,30 @@
   .controller('GoalCtrl', ['$scope', 'Goal', function($scope, Goal){
     $scope.goal = {};
     $scope.goals = [];
+    $scope.oneAtATime = true;
+
+    $scope.groups =[
+      {
+        title: 'Dynamic Group Header - 1',
+        content: 'Dynamic Group Body - 1'
+      },
+      {
+        title: 'Dynamic Group Header - 2',
+        content: 'Dynamic Group Body - 2'
+      }
+    ];
+
+    $scope.items =['Item 1', 'Item 2', 'Item 3'];
+
+    $scope.addItem = function(){
+      var newItemNo = $scope.items.length + 1;
+      $scope.items.push('Item ' + newItemNo);
+    };
+
+    $scope.status ={
+      isFirstOpen: true,
+      isFirstDisabled: false
+    };
 
     $scope.addGoal = function(){
       Goal.create($scope.goal).then(function(response){
