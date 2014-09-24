fe0f5804cd5689f20cc6adf3e3e985103eca1cfa
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-24 17:56:58 -0500

    tweak dashboard.js

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -2,7 +2,29 @@
   'use strict';
 
   angular.module('abode')
-  .controller('DashboardCtrl', ['$scope', function($scope){
+  .controller('DashboardCtrl', ['$scope', 'Goal', function($scope, Goal){
+    $scope.oneAtATime = true;
+    $scope.goals = [];
+    $scope.goal = {};
+
+    Goal.index().then(function(response){
+      $scope.goals = response.data.goals;
+    });
+
+    $scope.goalComplete = function(goal){
+      var goalId = goal._id,
+      index = _.indexOf($scope.goals, {_id:goalId});
+      Goal.goalComplete(goalId).then(function(response){
+        $scope.goals.splice(index, 1);
+      });
+    };
+
+    /*$scope.taskComplete = function(){
+      Goal.taskComplete().then(function(response){
+
+      });
+    };*/
+
   }]);
 })();
 
