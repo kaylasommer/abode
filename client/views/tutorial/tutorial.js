004d56dd23caaaa0e078bf8a34df9f98ad404f12
Kayla Jones <KaylaLyne424@gmail.com>
2014-10-11 18:46:38 -0500

    added table to dashboard

--- client/views/tutorial/tutorial.js
+++ client/views/tutorial/tutorial.js
@@ -73,46 +73,12 @@
       $scope.subForm = !!!$scope.subForm;
     };
 
-    $scope.addGoal = function(){
-      Goal.create($scope.goal).then(function(response){
-        $scope.goals.push(response.data.goal);
-        $scope.goal = {};
-      });
-    };
-
     $scope.sortableOptions = {
       stop: function(e, ui){
         var goal = ui.item.scope().$parent.$parent.goal;
         Goal.update(goal).then(function(){
         });
       }
-    };
-
-    $scope.addTask = function(goal){
-      goal.task.goalId = goal._id;
-      Goal.add(goal.task).then(function(response){
-        if (response.status === 200) {
-          goal.tasks.push(goal.task);
-        }
-        goal.task = {};
-      });
-    };
-
-    $scope.goalComplete = function(goal){
-      var goalId = goal._id,
-      index = _.indexOf($scope.goals, goal);
-      Goal.goalComplete(goalId).then(function(response){
-        $scope.user = response.data.user;
-        $scope.goals.splice(index, 1);
-      });
-    };
-
-    $scope.taskComplete = function(goal){
-      Goal.update(goal).then(function(){
-      },
-      function(err){
-        alert('Sorry, we aren\'t able to access the server at this time.');
-      });
     };
 
   }]);
